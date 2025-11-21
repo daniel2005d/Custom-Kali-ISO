1d7c5a9288c196c3854064a71902c2a8587852b5
daniel2005d <daniel2005d@gmail.com>
2025-11-21 10:16:17 -0500

    Sincronizacion

--- sync.sh
+++ sync.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 
-cp -r --v /home/daniel/live-build-config/kali-config/common/hooks/normal /home/daniel/Custom-Kali-ISO/kali-config/common/hooks/
+cp -r -v /home/daniel/live-build-config/kali-config/common/hooks/normal /home/daniel/Custom-Kali-ISO/kali-config/common/hooks/
+
+cp -r -v /home/daniel/live-build-config/kali-config/common/includes.binary/isolinux /home/daniel/Custom-Kali-ISO/kali-config/common/includes.binary/
+cp -r -v /home/daniel/live-build-config/kali-config/common/includes.chroot /home/daniel/Custom-Kali-ISO/kali-config/common/
+cp -r -v /home/daniel/live-build-config/kali-config/variant-default /home/daniel/Custom-Kali-ISO/kali-config/
+cp -r -v /home/daniel/live-build-config/kali-config/common/includes.installer /home/daniel/Custom-Kali-ISO/kali-config/common/
